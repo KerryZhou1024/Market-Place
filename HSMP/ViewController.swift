53ff877942c9e2e274dd0cae465b5bafa921c6be
Kerry Zhou <cosmosedition@gmail.com>
2020-10-06 16:33:28 -0400

    Initial Commit

---
+++ HSMP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSMP
+//
+//  Created by Kerry Zhou on 10/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
